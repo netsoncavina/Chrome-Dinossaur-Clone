6af32cbefa6b4db5566b3ecc4d18d60e836e0eb4
Netson <netsoncavina@gmail.com>
2021-08-10 14:06:54 -0300

    Adição de gravidade e mecânica de pulo

--- runner_class.py
+++ runner_class.py
@@ -26,6 +26,8 @@ snail_rect = snail_surface.get_rect(midbottom = (600,300))
 
 player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80,300)) # Cria um retângulo em volta da imagem e define o ponto de origem
+player_gravity = 0
+
 
 
 while True: # Tudo que é mostrado e atualizado, fica dentro dessa condição
@@ -33,6 +35,16 @@ while True: # Tudo que é mostrado e atualizado, fica dentro dessa condição
         if event.type == pygame.QUIT:
             pygame.quit() # Finaliza o .init()
             exit() # Para finalizar sem erro
+
+        if event.type == pygame.MOUSEBUTTONDOWN: # Mecânica de pulo, mas com o mouse 
+            if player_rect.collidepoint(event.pos):
+                player_gravity -= 5
+
+        if event.type == pygame.KEYDOWN: # Verifica se um botão do teclado foi pressionado
+            if event.key == pygame.K_SPACE: # Verifica se o botão pressionado foi a barra de espaço 
+                player_gravity -= 5
+        # if event.type == pygame.KEYUP: # Verifica se o botão não está mais sendo pressionado
+        #     player_rect.bottom += 20
     screen.blit(sky_surface,(0,0)) # Coloca uma superficie sobre a outra (superficie,posição) 
     screen.blit(ground_surface,(0,300))
 
@@ -45,12 +57,19 @@ while True: # Tudo que é mostrado e atualizado, fica dentro dessa condição
     snail_rect.x -= 3
     if snail_rect.right <= 0:
         snail_rect.left = 820
-       
+
+    # Snail   
     screen.blit(snail_surface,snail_rect)
+    
+    
+    # Player
+    player_gravity += 0.05
+    player_rect.y += player_gravity
     screen.blit(player_surface,player_rect)
 
-    if player_rect.colliderect(snail_rect):
-        print("Colidiu")
+
+    # if player_rect.colliderect(snail_rect):
+    #     print("Colidiu")
 
 
     pygame.display.update()
